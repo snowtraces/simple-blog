e20351c37b94e7aece215aa6ed825bb602078905
chengxinyong <chengxinyong@139.com>
2020-08-13 16:46:28 +0800

    项目初始化

---
+++ js/main.js
@@ -0,0 +1,37 @@
+var jsList = [
+    './js/util/function.js',
+    './js/util/event-hub.js',
+]
+
+var developModel = false;
+
+var cssList = [
+    './css/main.css'
+]
+
+var version = developModel ? new Date().getTime() : '1.0.0';
+
+function loadScript(url) {
+    var script = document.createElement('script');
+    script.src = `${url}?version=${version}`;
+    var body = document.querySelector('body');
+    body.append(script);
+}
+function loadCss(url) {
+    var link = document.createElement('link');
+    link.rel = 'stylesheet'
+    link.href = `${url}?version=${version}`;
+    var head = document.querySelector('head');
+    head.append(link);
+}
+
+window.onload = function () {
+    cssList.forEach(css => {
+        this.loadCss(css)
+    })
+
+    jsList.forEach(js => {
+        loadScript(js)
+    })
+
+}
